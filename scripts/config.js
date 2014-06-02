20915a8f85c4a8d4fae378eca3622808e14ffccf
masih <masiih@gmail.com>
2014-06-02 11:51:43 +0100

    fixed cross origin issue when loading default config

--- scripts/config.js
+++ scripts/config.js
@@ -16,9 +16,27 @@ define(['jquery', 'jsyaml'], function ($, yaml) {
         return config_json;
     }
 
+    function getDefaultConfiguration() {
+        var default_config_path = "https://api.github.com/repos/masih/benevis/contents/default-structure.yml";
+        var response;
+        $.ajax({
+            type: 'GET',
+            url: default_config_path,
+            async: false,
+            headers: {
+                "Accept": "application/vnd.github.v3.raw"
+            }
+        }).done(function (data) {
+            response = data;
+        }).fail(function (error) {
+            console.log('failed to load the default configuration', error);
+        });
+
+        return yaml.safeLoad(response);
+    }
 
     var config = getYAML("structure.yml");
-    var default_config = getYAML("https://masih.github.io/benevis/default-structure.yml");
-    var extended_config = $.extend({}, default_config, config);
-    return extended_config;
+
+    var default_config = getDefaultConfiguration();
+    return $.extend({}, default_config, config);
 });
